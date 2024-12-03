9f1c6e5b612b90e23b8dc22897078ff2a960b14c
rfdouro <rfdouro@gmail.com>
2024-12-03 15:07:33 -0300

    codigo: PUSH exemplo vue3

---
+++ android/app/src/main/java/com/rfdouro/vcap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rfdouro.vcap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+ <main>
+
+  <button @click="teste">TESTE</button>
+ </main>
+</template>
+
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+
+function teste() {
+ alert(123);
+}
+</script>
