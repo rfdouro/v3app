07d233143f05b255982f2a5f9e42edfd3bd61a85
rfdouro <rfdouro@gmail.com>
2024-12-04 09:46:54 -0300

    teste pwa 2024_12_04_ 9:46

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-  ? './' // prod
-  : '/', // dev,
+  base:
+    process.env.NODE_ENV === "production"
+      ? "./" // prod
+      : "/", // dev,
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
